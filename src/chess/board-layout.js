9f72f6d3847c508fb1e2e08265d3b35f746d228d
Jildert <jildert.venema@hotmail.com>
2021-02-13 11:08:12 +0100

    reset board layoutr

--- src/chess/board-layout.js
+++ src/chess/board-layout.js
@@ -1,71 +1,42 @@
 export default {
-  3: [
-    "",
-    "",
+  0: [
+    "black-rook",
     "black-knight",
+    "black-bishop",
+    "black-queen",
+    "black-king",
+    "black-bishop",
     "black-knight",
-    "black-knight",
-    "black-knight",
-    "black-knight",
-    ""
+    "black-rook"
   ],
-  1: [],
-  6: [],
-  5: [
-    "",
-    "",
-    "",
-    "",
-    // "black-rook",
-    // "black-knight",
-    // "black-bishop",
-    // "black-queen",
+  1: [
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn"
+  ],
+  6: [
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
     "white-pawn"
-    // "black-bishop",
-    // "black-knight",
-    // "black-rook"
+  ],
+  7: [
+    "white-rook",
+    "white-knight",
+    "white-bishop",
+    "white-queen",
+    "white-king",
+    "white-bishop",
+    "white-knight",
+    "white-rook"
   ]
 };
-
-// export default {
-//   0: [
-//     "black-rook",
-//     "black-knight",
-//     "black-bishop",
-//     "black-queen",
-//     "black-king",
-//     "black-bishop",
-//     "black-knight",
-//     "black-rook"
-//   ],
-//   1: [
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn",
-//     "black-pawn"
-//   ],
-//   6: [
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn",
-//     "white-pawn"
-//   ],
-//   7: [
-//     "white-rook",
-//     "white-knight",
-//     "white-bishop",
-//     "white-queen",
-//     "white-king",
-//     "white-bishop",
-//     "white-knight",
-//     "white-rook"
-//   ]
-// };
